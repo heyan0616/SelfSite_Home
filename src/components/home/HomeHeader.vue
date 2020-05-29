5ad1cdaa06d659a0f1afe3b046a52faa05b9e37c
Yan He <heyan-0616@163.com>
2020-05-29 12:47:09 +0800

    update

--- src/components/home/HomeHeader.vue
+++ src/components/home/HomeHeader.vue
@@ -19,9 +19,9 @@
                         <iframe src="http://wufazhuce.com/one/2832" width="854" height="180" frameborder="0" scrolling="no"
                         ></iframe>
                     </div> -->
-                    <div style="width:800px; height:180px; overflow:hidden; border:0px">
-                        <div style="width:800px; height:1000px;margin:-679px 10px 0px 0px;">
-                            <iframe v-bind:src="searchUrl" width="800" height="1000" frameborder="0" scrolling="no"
+                    <div style="width:760px; height:180px; overflow:hidden; border:0px;">
+                        <div style="width:760px; height:1000px;margin:-679px 0px 0px 0px;">
+                            <iframe v-bind:src="searchUrl" width="770" height="900" frameborder="0" scrolling="no"
                             ></iframe>
                         </div>
                     </div>
@@ -120,7 +120,8 @@ export default {
 <style scoped>
     .header-section{
         float : left;
-        width: 30%;
+        width: 35%;
+        padding-bottom: 10px;
     }
     .banner-text-w3ls h2{
         color: #01aef0;
