7b43c4593208c5c3ce994b7056481b47420db1a4
Yan He <heyan-0616@163.com>
2020-06-14 14:49:26 +0800

    update

--- builds/release.sh
+++ builds/release.sh
@@ -9,4 +9,6 @@ echo "start scp files from /vue_workspace/vue-home/dist/* to root@heyan.site:/ho
 sshpass -p "Heyan0616" ssh root@heyan.site "rm -rf /home/nginx_homepage/html/*"
 echo "clean the remote server html folder complete"
 sshpass -p "Heyan0616" scp -r /Users/heyan/self/workspace/vue_workspace/vue-home/dist/* root@heyan.site:/home/nginx_homepage/html
+echo "start copy downloads folder in remote server"
+sshpass -p "Heyan0616" ssh root@heyan.site "cp -r /home/nginx_homepage/downloads/ /home/nginx_homepage/html/"
 echo "scp files to server complete"
---
+++ downloads/README.md
@@ -0,0 +1,3 @@
+Note：
+
+/downloads 自建目录，存放用于在网站download的文件 - 不属于项目目录
